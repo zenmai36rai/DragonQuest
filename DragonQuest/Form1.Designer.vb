fe07af3198c3ae0787cef6f86beb2be0deae83af
Kyosuke Miyazawa <zenmai36rai@gmail.com>
2024-09-14 13:02:01 +0900

    ドラクエのハローワールドはスライムとの出会いから

--- DragonQuest/Form1.Designer.vb
+++ DragonQuest/Form1.Designer.vb
@@ -22,10 +22,59 @@ Partial Class Form1
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.DragonQuest.My.Resources.Resources.スライム
+        Me.PictureBox1.Location = New System.Drawing.Point(270, 111)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(233, 236)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.BackColor = System.Drawing.Color.Black
+        Me.RichTextBox1.ForeColor = System.Drawing.Color.White
+        Me.RichTextBox1.Location = New System.Drawing.Point(13, 353)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.ReadOnly = True
+        Me.RichTextBox1.Size = New System.Drawing.Size(775, 143)
+        Me.RichTextBox1.TabIndex = 1
+        Me.RichTextBox1.Text = ""
+        '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.BackColor = System.Drawing.Color.Black
+        Me.RichTextBox2.ForeColor = System.Drawing.Color.White
+        Me.RichTextBox2.Location = New System.Drawing.Point(13, 15)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.ReadOnly = True
+        Me.RichTextBox2.Size = New System.Drawing.Size(775, 90)
+        Me.RichTextBox2.TabIndex = 2
+        Me.RichTextBox2.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(806, 512)
+        Me.Controls.Add(Me.RichTextBox2)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents RichTextBox2 As RichTextBox
 End Class
--- DragonQuest/Form1.vb
+++ DragonQuest/Form1.vb
@@ -1,3 +1,10 @@
 ﻿Public Class Form1
-
+    Private BATTLE_LOG As String
+    Private PLAYER_STATUS As String
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PLAYER_STATUS = "+-- ゆうしゃ --+" + vbCrLf + "|     HP 99     |" + vbCrLf + "|     MP 99     |" + vbCrLf + "+-----------+"
+        BATTLE_LOG = "+----------------+" + vbCrLf + "| スライムがあらわれた |" + vbCrLf + "+----------------+"
+        RichTextBox1.Text = BATTLE_LOG
+        RichTextBox2.Text = PLAYER_STATUS
+    End Sub
 End Class
